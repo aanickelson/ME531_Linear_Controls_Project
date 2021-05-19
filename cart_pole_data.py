603d0baaa31a5c336d159f77a6f1576464399468
Anna Nickelson <aanickelson@gmail.com>
2021-05-19 16:52:07 -0700

    Updates to NN, data gathering, data testing, and data collected

--- cart_pole_data.py
+++ cart_pole_data.py
@@ -3,40 +3,86 @@
 import gym
 import numpy as np
 import time
+from cart_pole_nn import create_a_nn, use_trained_nn
 
 
 def generate_data(episodes):
+    threshold = 90
     env = gym.make('CartPole-v0')
+    num_data_points = 0
 
-    all_data = np.array([])
+    all_data = []
     for episode in range(episodes):
         observation = env.reset()
         episode_data = []
-        if not episode % 100:
+        reward = 0
+        if not episode % 1000:
             print("Episode ", episode)
+            print("Data points:", num_data_points)
         for t in range(200):
             # env.render()
             action = env.action_space.sample()
             observation, reward, done, info = env.step(action)  # take a random action
-
+            if action == 0:
+                action = -1
             observation = np.append(observation, action)
             # observation is now [x, xdot, theta, thetadot, action (0 or 1)]
             episode_data.append(observation)
 
             if done:
-                reward = np.array([[t + 1] for _ in range(t+1)])
-                episode_data = np.array(episode_data)
-                episode_data = np.append(episode_data, reward, axis=1)
-                if episode == 0:
-                    all_data = episode_data.copy()
-                # print(episode_data)
+                reward = t + 1
                 break
-        if episode > 0:
-            all_data = np.vstack((all_data, episode_data))
+
+        if reward >= threshold:
+            num_data_points += 1
+            for obs in episode_data:
+                all_data.append(obs)
+                # print(len(all_data))
+            # print(all_data)
+        env.close()
+
+    all_data = np.array(all_data)
+    print(all_data)
+    np.save('cart-pole-data-{}'.format(time.strftime("%Y%m%d-%H%M%S")), all_data)
+
+
+def use_nn_for_cartpole(network, episodes):
+    env = gym.make('CartPole-v0')
+
+    all_data = np.array([])
+    for episode in range(episodes):
+        observation = env.reset()
+        observation = np.reshape(observation, (1, 4))
+
+        if not episode % 1000:
+            print("Episode ", episode)
+
+        for t in range(200):
+            # env.render()
+            action = network.predict(observation)
+            if action > 0.0:
+                action = 1
+            else:
+                action = 0
+
+            observation, reward, done, info = env.step(action)  # take a random action
+            observation = np.reshape(observation, (1, 4))
+
+            if done:
+                print("Network achieved {} time steps".format(t+1))
+
+                break
         env.close()
 
     np.save('cart-pole-data-{}'.format(time.strftime("%Y%m%d-%H%M%S")), all_data)
 
 
+def test_nn():
+    nn_weights_file = "weights-20210519-143022"
+    network = use_trained_nn(nn_weights_file)
+    use_nn_for_cartpole(network, 100)
+
+
 if __name__ == "__main__":
-    generate_data(100000)
+    # generate_data(500000)
+    test_nn()
--- cart_pole_nn.py
+++ cart_pole_nn.py
@@ -1,17 +1,43 @@
 #!/usr/bin/env python3
 
 import numpy as np
-# from keras import models
-# from keras import layers
+import tensorflow as tf
+import time
+
+
+def create_a_nn():
+    network = tf.keras.Sequential()
+    network.add(tf.keras.layers.Dense(6, input_shape=(4,)))
+    network.add(tf.keras.layers.Dense(1))
+    network.compile(optimizer='Adam', loss='mean_squared_error')
+    return network
+
+
+def train_the_nn(data_in, data_out):
+    weights_path = 'weights-{}'.format(time.strftime("%Y%m%d-%H%M%S"))
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=weights_path,
+                                                     save_weights_only=True,
+                                                     verbose=1)
+    model = create_a_nn()
+    model.fit(data_in, data_out, epochs=5, callbacks=[cp_callback])
+
+
+def use_trained_nn(weights_file):
+    model = create_a_nn()
+    model.load_weights(weights_file)
+    return model
 
 
 if __name__ == "__main__":
 
-    all_data = np.load("cart-pole-data-20210518-160517.npy")
+    all_data = np.load("cart-pole-data-20210519-142552.npy")
 
     # inputs = all_data[:, :4]
-    outputs = all_data[:, 4:5]
-    inputs = np.delete(all_data, 4, axis=1)
+    all_out = all_data[:, 4:]
+    all_in = np.delete(all_data, 4, axis=1)
 
-    print(inputs)
-    print(outputs)
+    # print(all_in)
+    # print(all_out)
+
+    train_the_nn(all_in, all_out)
+
